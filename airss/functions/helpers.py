7c7ab3fdf5c0ba1d7627c3b025ad56886606146a
Rami Borni <rami@scribfy.app>
2024-05-19 14:30:14 +0100

    improve date parser

--- airss/functions/helpers.py
+++ airss/functions/helpers.py
@@ -3,14 +3,19 @@ import pytz
 
 
 def parse_published_date(date_string):
-    date_formats = ['%a, %d %b %Y %H:%M:%S GMT', '%a, %d %b %Y %H:%M:%S %z', '%a, %d %b %Y %H:%M:%S %z']
+    date_formats = [
+        '%a, %d %b %Y %H:%M:%S GMT',
+        '%a, %d %b %Y %H:%M:%S %z'
+    ]
     norway_timezone = pytz.timezone('Europe/Oslo')
 
     for date_format in date_formats:
         try:
             datetime_obj = datetime.strptime(date_string, date_format)
-            datetime_obj = datetime_obj.astimezone(norway_timezone)  # Convert to Oslo time zone
+            datetime_obj = datetime_obj.replace(tzinfo=pytz.utc).astimezone(norway_timezone)  # Convert to Oslo time zone
             return datetime_obj
-        except Exception as e:
+        except ValueError:
             pass
+
+    # Return current time in Oslo timezone if parsing fails
     return datetime.now().astimezone(norway_timezone)
